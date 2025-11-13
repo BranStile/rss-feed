d7641939379b4f81e60c5c3b55a56d2e00b814c1
Brandon Stile <bgstile06@gmail.com>
2025-11-12 20:43:38 -0500

    Gridded and Sized
    Made the cards appear in a grid as well as the grid changing columns when screen size changes

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,18 @@ interface FeedItem {
 
 setInterval(refresh, 300000);
 
+export function screenSize() {
+  const screenSize = screen.width;
+  if (screenSize > 1050) {
+    return 3;
+  }
+  else if (screenSize <= 1050) {
+    return 2;
+  }
+  else {
+    return 1;
+  }
+}
 
 export default function FeedList() {
   const [items, setItems] = useState<FeedItem[]>([]);
@@ -22,18 +34,18 @@ export default function FeedList() {
   let min: string = "";
   let second: string = "";
 
-  if(now.getSeconds()/10 < 1){
+  if (now.getSeconds() / 10 < 1) {
     second = `0${now.getSeconds()}`;
   }
 
-  else{
+  else {
     second = `${now.getSeconds()}`;
   }
 
-  if(now.getMinutes()/10 < 1){
+  if (now.getMinutes() / 10 < 1) {
     min = "0" + min;
   }
-  else{
+  else {
     min = `${now.getMinutes()}`;
   }
 
@@ -43,37 +55,37 @@ export default function FeedList() {
       .then(setItems)
       .catch(console.error);
   }, []);
-
   return (
     <div>
-
       <div className="p-4">
         <h1 className="header">News Feed</h1>
-        <p className="text-center">Last Refreshed: {now.getDate()}/{now.getMonth()+1}/{now.getFullYear()} {now.getHours()%12}:{min}:{second}</p>
-        {items.map((item, i) => (
-          <div key={i} className="border-b padd">
-            <div className= "test">
-            <a
-              href={item.link}
-              className=" center card preset-filled-surface-100-900 border border-surface-200-800 card-hover divide-surface-200-800 block test divide-y overflow-hidden"
-            >
-              <article className="space-y-4 p-4">
-                <div>
-                  <h2 className="h6">{item.published.substring(0,25)}</h2>
-                  <h3 className="h3">{item.title}</h3>
-                </div>
-                <p className="opacity-60">
-                  {item.summary}
-                </p>
-              </article>
-              {/* Footer */}
-              <footer className="flex items-center justify-between gap-4 p-4">
-                <small className="opacity-60">White House</small>
-              </footer>
-            </a>
-          </div>
-          </div>
-        ))}
+        <p className="text-center">Last Refreshed: {now.getDate()}/{now.getMonth() + 1}/{now.getFullYear()} {now.getHours() % 12}:{min}:{second}</p>
+        <div className="grid">
+          {items.map((item, i) => (
+            <div key={i} className=" border-b padd">
+              <div className="test">
+                <a
+                  href={item.link}
+                  className="card preset-filled-surface-100-900 border border-surface-200-800 card-hover divide-surface-200-800 block divide-y overflow-hidden"
+                >
+                  <article className="space-y-4 p-4">
+                    <div>
+                      <h2 className="h6">{item.published.substring(0, 25)}</h2>
+                      <h3 className="h3">{item.title}</h3>
+                    </div>
+                    <p className="opacity-60">
+                      {item.summary}
+                    </p>
+                  </article>
+                  {/* Footer */}
+                  <footer className="flex items-center justify-between gap-4 p-4">
+                    <small className="opacity-60">White House</small>
+                  </footer>
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
