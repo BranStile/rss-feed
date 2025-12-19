5241208e9f43649444461c16ddc68ada5f344a52
Brandon Stile <bgstile06@gmail.com>
2025-12-18 23:08:28 -0500

    Update grid layout and app bar; started context
    Changed grid columns from 5 to 3 and added text color in app.css. Updated appbar to use new title, colors, and buttons. Added LevelContext in context.js for shared state management.

--- app/appbar.tsx
+++ app/appbar.tsx
@@ -2,26 +2,25 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import {Link} from 'react-router-dom'
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import React from 'react';
+import { red, yellow } from '@mui/material/colors';
 
 const MyAppBar = () => {
   return (
-    <AppBar position="static"> {/* "static" for normal flow, "fixed" to stick to top */}
-      <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
+    <AppBar position="static" sx={{bgcolor: '#8e9e8e'}}>
+      <Toolbar >
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          MyApp Title
+          StockNews
         </Typography>
+        <Button  color="inherit">Whitehouse.gov</Button>
+        {/* put in context */}
+        <Button color="inherit" sx = {{flexGrow: .2}}>TruthSocial</Button>
+
+        {/* <Button component = {Link} to = "/social"color="inherit" sx = {{flexGrow: .2}}>TruthSocial</Button> */}
+        
         <Button color="inherit">Login</Button>
       </Toolbar>
     </AppBar>
